ece706f0ce4864d5c242004e0bf4424c20c5e0d5
HannahAraullo <araullohannahjoy@gmail.com>
2020-10-05 10:37:58 +0800

    Second Commit

--- CollectionsApp/Form1.Designer.cs
+++ CollectionsApp/Form1.Designer.cs
@@ -36,38 +36,38 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(196, 276);
+            this.button1.Location = new System.Drawing.Point(123, 276);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 53);
+            this.button1.Size = new System.Drawing.Size(362, 53);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 335);
+            this.button2.Location = new System.Drawing.Point(123, 335);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 53);
+            this.button2.Size = new System.Drawing.Size(362, 53);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "REMOVE";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(196, 28);
+            this.textBox1.Location = new System.Drawing.Point(123, 28);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.Size = new System.Drawing.Size(362, 22);
             this.textBox1.TabIndex = 2;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(196, 56);
+            this.listBox1.Location = new System.Drawing.Point(123, 80);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(200, 148);
+            this.listBox1.Size = new System.Drawing.Size(362, 180);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -75,7 +75,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(598, 450);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
